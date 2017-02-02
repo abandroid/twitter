58d795007735687379c96f10ace985a46d3efc80
Jan Ooms <jan@creatiom.be>
2017-02-02 12:44:58 +0100

    Boolean Node instead of integer node

--- src/Bundle/DependencyInjection/Configuration.php
+++ src/Bundle/DependencyInjection/Configuration.php
@@ -31,7 +31,7 @@ class Configuration implements ConfigurationInterface
                     ->scalarNode('api_url')->defaultValue(null)->end()
                     ->scalarNode('proxy')->defaultValue(null)->end()
                     ->integerNode('timeout')->defaultValue(5)->end()
-                    ->integerNode('verify_peer')->defaultValue(true)->end()
+                    ->booleanNode('verify_peer')->defaultValue(true)->end()
                 ->end()
         ;
 
